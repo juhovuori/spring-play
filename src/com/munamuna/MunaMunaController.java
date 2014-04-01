b7436fa7b1309185f01379ea8fe2a09136935637
Juho Vuori <juho.vuori@helsinki.fi>
2014-04-01 14:59:06 +0300

    something more

--- src/com/munamuna/MunaMunaController.java
+++ src/com/munamuna/MunaMunaController.java
@@ -1,9 +1,10 @@
 package com.munamuna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.ui.ModelMap;
 
 @Controller
 @RequestMapping("/")
@@ -11,7 +12,35 @@ public class MunaMunaController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printHello(ModelMap model) {
-		model.addAttribute("message", "Hello Spring MVC Framework!");
+		model.addAttribute("message", "Munamuna messageboard");
+
+		return "muna";
+	}
+
+	@RequestMapping(value = "/muna", method = RequestMethod.POST)
+	public String postMuna(@PathVariable("id") long id, ModelMap model) {
+		model.addAttribute("message", "Munamuna messageboard");
+
+		return "muna";
+	}
+
+	@RequestMapping(value = "/muna/{id}", method = RequestMethod.GET)
+	public String getMuna(@PathVariable("id") long id, ModelMap model) {
+		model.addAttribute("message", "Munamuna messageboard");
+
+		return "muna";
+	}
+
+	@RequestMapping(value = "/muna/{id}", method = RequestMethod.PUT)
+	public String putMuna(@PathVariable("id") long id, ModelMap model) {
+		model.addAttribute("message", "Munamuna messageboard");
+
+		return "muna";
+	}
+
+	@RequestMapping(value = "/muna/{id}", method = RequestMethod.DELETE)
+	public String deleteMuna(@PathVariable("id") long id, ModelMap model) {
+		model.addAttribute("message", "Munamuna messageboard");
 
 		return "muna";
 	}
